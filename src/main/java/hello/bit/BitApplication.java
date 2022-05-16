d66e949998134d7eccfd67a6e31826998006785d
mana <minu960604@gmailc.om>
2022-05-16 16:28:55 +0900

    2022-05-16

---
+++ src/main/java/hello/bit/BitApplication.java
@@ -0,0 +1,13 @@
+package hello.bit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/bit/BitApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.bit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
